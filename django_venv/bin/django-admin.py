e4e7536d2f38c9fdfa0057022c2e513cff613692
abhisek-technicise <abhisekroy.cse@gmail.com>
2017-07-03 11:23:06 +0530

    Successful installation of Django and Django Piston

---
+++ django_venv/bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/user/code/Django-Sample/django_venv/bin/python2.7
+# EASY-INSTALL-SCRIPT: 'Django==1.11.3','django-admin.py'
+__requires__ = 'Django==1.11.3'
+__import__('pkg_resources').run_script('Django==1.11.3', 'django-admin.py')
